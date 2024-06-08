5ed9037ce72587c2800ac0c05be56e9d39238852
ItisCaleb <40360126+ItisCaleb@users.noreply.github.com>
2024-06-08 23:56:25 +0800

    Add a custom popup component

--- src/main/java/com/itiscaleb/cpcompound/component/EditorPopup.java
+++ src/main/java/com/itiscaleb/cpcompound/component/EditorPopup.java
@@ -3,6 +3,9 @@ package com.itiscaleb.cpcompound.component;
 import javafx.animation.PauseTransition;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.control.PopupControl;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Popup;
 import javafx.stage.Window;
 import javafx.util.Duration;
@@ -10,29 +13,71 @@ import javafx.util.Duration;
 
 public class EditorPopup extends Popup {
 
-    PauseTransition timer = new PauseTransition(Duration.seconds(3));
+    PauseTransition timer;
     static final String labelStyle =
             "-fx-background-color: black;" +
             "-fx-text-fill: white;" +
             "-fx-padding: 5";
 
     Label label;
-    public EditorPopup(){
+    boolean should_hide = true;
+
+
+    public EditorPopup() {
+        this(1);
+    }
+
+
+    public EditorPopup(double hideSeconds){
         super();
+        timer = new PauseTransition(Duration.seconds(hideSeconds));
+        // make label copyable
         label = new Label();
         label.setStyle(labelStyle);
         this.getContent().add(label);
+        label.addEventHandler(MouseEvent.MOUSE_ENTERED, e -> {
+            should_hide = false;
+        });
+
+        label.addEventHandler(MouseEvent.MOUSE_EXITED, e -> {
+            should_hide = true;
+            timer.playFromStart();
+        });
         timer.setOnFinished(event -> {
-            System.out.println(label.isFocused());
-            if(label.isFocused()){
-               timer.playFromStart();
-            }else this.hide();
+            if(this.should_hide){
+               super.hide();
+            }
         });
     }
 
     public void setText(String str){
         label.setText(str);
-        //timer.playFromStart();
     }
 
+    // If autoHide is true, then this popup will hide seconds after lost hover
+    @Override
+    public void show(Node ownerNode, double anchorX, double anchorY) {
+        super.show(ownerNode, anchorX, anchorY);
+        timer.pause();
+        if(this.isAutoHide()){
+            hide();
+        }
+    }
+
+    // If autoHide is true, then this popup will hide seconds after lost hover
+    @Override
+    public void show(Window ownerWindow, double anchorX, double anchorY) {
+        super.show(ownerWindow, anchorX, anchorY);
+        timer.pause();
+        if(this.isAutoHide()){
+            hide();
+        }
+    }
+
+    // Hide this popup seconds after lost hover
+    @Override
+    public void hide() {
+        this.should_hide = true;
+        timer.playFromStart();
+    }
 }
