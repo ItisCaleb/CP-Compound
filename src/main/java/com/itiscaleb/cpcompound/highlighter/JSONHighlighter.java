7fbf4e254be4a2b14cc2a0c2bc1246b2c02d94fe
ItisCaleb <40360126+ItisCaleb@users.noreply.github.com>
2024-06-08 13:02:58 +0800

    Fix json highlight

--- src/main/java/com/itiscaleb/cpcompound/highlighter/JSONHighlighter.java
+++ src/main/java/com/itiscaleb/cpcompound/highlighter/JSONHighlighter.java
@@ -8,23 +8,29 @@ import java.util.List;
 import java.util.regex.Pattern;
 
 public class JSONHighlighter extends Highlighter {
-
-    private static final String NUMBER_PATTERN = Highlighter.NUMBER_PATTERN + "(?:[eE][+-]?\\d+)?[fF]?\\b"
-            + "|" + "\\b-?0x[\\da-fA-F]+\\b" + "|" + "\\b-?0b[01]+\\b";
+    private static final String[] KEYWORDS = new String[] {
+      "true", "false", "null",
+    };
+    private static final String KEYWORD_PATTERN ="\\b(" + String.join("|", KEYWORDS) + ")\\b";
+    private static final String KEY_PATTERN = STRING_PATTERN + ":";
+    private static final String NUMBER_PATTERN = Highlighter.NUMBER_PATTERN + "(?:[eE][+-]?\\d+)?\\b";
 
     private static final Pattern PATTERN = Pattern.compile(
-            "|(?<BRACE>" + BRACE_PATTERN + ")"
+            "(?<KEYWORD>" + KEYWORD_PATTERN + ")"
+                    + "|(?<BRACE>" + BRACE_PATTERN + ")"
                     + "|(?<BRACKET>" + BRACKET_PATTERN + ")"
+                    + "|(?<KEY>" + KEY_PATTERN + ")"
                     + "|(?<STRING>" + STRING_PATTERN + ")"
                     + "|(?<NUMBER>" + NUMBER_PATTERN + ")"
 
     );
     private static final List<Pair<String,String>> GROUP = new ArrayList<>(
             Arrays.asList(
+                    new Pair<>("KEYWORD", "json-keyword"),
                     new Pair<>("BRACE", "brace"),
                     new Pair<>("BRACKET", "bracket"),
+                    new Pair<>("KEY", "json-key"),
                     new Pair<>("STRING", "string"),
-                    new Pair<>("COMMENT", "comment"),
                     new Pair<>("NUMBER", "number"))
     );
 
